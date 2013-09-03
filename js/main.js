2dc772f876a790de1023565f91c984e971b96ead
Vladimir Minkin <vladimir.minkin@gmail.com>
2013-09-03 11:32:36 +0400

    New Index Page

--- js/main.js
+++ js/main.js
@@ -1,16 +1,16 @@
 (function () {
 	var params = {
 		sizes : [
-			{ min:0,     max:380,   cols: 1,    fixed: true },
-			{ min:380,   max:760,   cols: 2,    fixed: true },
-			{ min:760,   max:1000,  cols: 4  },
-			{ min:1000,  max:1280,  cols: 6  },
-			{ min:1280,  max:1600,  cols: 8  },
-			{ min:1600,  max:1920,  cols: 10 },
-			{ min:1920,  max:2680,  cols: 12 }
+			{ 	min:0,     max:380,   cols: 1,    fixed: true },
+			{ 	min:380,   max:760,   cols: 2,    fixed: true },
+			{ 	min:760,   max:1000,  cols: 4  },
+			{ 	min:1000,  max:1280,  cols: 6  },
+			{ 	min:1280,  max:1600,  cols: 8  },
+			{ 	min:1600,  max:1920,  cols: 10 },
+			{ 	min:1920,  max:2680,  cols: 12 }
 		],
-		max_width_dimension 	: 2,
-		max_height_dimension 	: 2,
+		max_width_dimension 	: 1,
+		max_height_dimension 	: 1,
 		static_proportion 		: false, // "1x1"
 		insequense				: false, // fit last tile in row to cell width 
 		squarescale				: true, // scale tiles proportionally when resize
